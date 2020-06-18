9e657eb2da7fc1dc9ea4c6565610853ebc4776a9
Marcin Wojtkowski <45155952+marcin-wojtkowski@users.noreply.github.com>
2020-06-18 15:23:12 -0700

    Added a simple GUI

--- main.py
+++ main.py
@@ -2,16 +2,26 @@
     Stage 1
         Download a webpage. Save the webpage data on my PC. Store it in a file.
         Have the format be identical to its online format.
-
+           
+    Stage 2  
+        Add PySimpleGUI as a low level UI
 """
 
 import requests
 from bs4 import BeautifulSoup
+import PySimpleGUI as sg
+sg.theme('Topanga')
 
 
-webpage_url = input()
+layout = [  [sg.Text('Url to download: '), sg.InputText(key='-URL-')],
+            [sg.Submit()]]
 
-webpage = requests.get(webpage_url)
+window = sg.Window('URL Downloader', layout)
+
+event, values = window.read()
+
+
+webpage = requests.get(values['-URL-'])
 
 fwebpage_name = webpage_url.replace('/','_') + '.html'
 
@@ -19,4 +29,5 @@ soup = BeautifulSoup(webpage.text, 'lxml')
 
 with open(fwebpage_name, 'w') as f:
     f.write(soup.prettify())
-#SUCCESS! Offline parsing is now possible. The entire webpage is saved locally. 
+
+    
